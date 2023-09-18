1e6f2586bbea2df5e0dfc7ce7b59319e3203036a
Özay Melih Yıldız <ozay.m.yildiz@gmail.com>
2023-09-18 20:40:33 +0300

    a

---
+++ Capsul/Capsul/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            student.setName("Özay");
+            Console.WriteLine(student.getName());
+
+            student.HEIGHT = 50;
+            Console.WriteLine(student.HEIGHT);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Capsul/Capsul/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capsul
+{
+    public class Student
+    {
+        private string name;
+        private int height;
+
+        public int HEIGHT
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
